a450f11d5c960cc59252c8e5a93d2e16047c190b
Julian CARLE <carle.julian@gmail.com>
2020-07-10 14:38:59 +0200

    3 different functions for reverse string

---
+++ reverse.js
@@ -0,0 +1,21 @@
+// REVERSE STRING
+function reverse1(str){
+    if(!str || typeof str != 'string' || str.length < 2 ) return str;
+    const backwords = [];
+    const totalItems = str.length - 1;
+    for(let i = totalItems; i >= 0; i--){
+        backwords.push(str[i]);
+    }
+    return backwords.join('');
+}
+  
+function reverse2(str){
+//check for valid input
+return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
+
+reverse1('Hello World');
+reverse('Hello World');
+reverse3('Hello World');
